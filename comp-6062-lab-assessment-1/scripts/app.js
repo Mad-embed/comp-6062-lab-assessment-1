e8a9ee7bcfa4335fc508c96b8d1e448febe525b1
Mad-embed <madhusudhanreddy32313@gmail.com>
2025-02-13 19:55:20 -0500

    done
    done

---
+++ comp-6062-lab-assessment-1/scripts/app.js
@@ -0,0 +1,15 @@
+
+const studentFullName= "Madhusudhan";
+const studentNumber= "1157281";
+console.log('${studentFullName}-${studentNumber}'); 
+// Do not edit below this line doing so will break the project, this code will display the values on the page
+//
+
+
+const headerContent = document.querySelector("h1");
+headerContent.innerHTML = `${studentFullName}-${studentNumber}`;
+
+headerContent.classList.add(headingPrimary);
+    
+
+//2025 Build
